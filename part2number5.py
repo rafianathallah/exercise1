c46bb47f1a22c11e4212b7d248993f67caf3b1b4
rafianbinus <71361919+rafianbinus@users.noreply.github.com>
2020-09-21 12:30:11 +0700

    Add files via upload

---
+++ part2number5.py
@@ -0,0 +1,6 @@
+velocity = float(input("Enter speed (m/s): "))
+frequency = float(input("Enter frequency (Hz): "))
+wavelength = velocity / frequency
+print("The Speed (m/s):",velocity)
+print("The frequency (Hz):",frequency)
+print("The wavelength (m):",wavelength)
